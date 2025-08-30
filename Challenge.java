a5f58ae16484c62bfd6af5b4de8334b9c7aafcde
CheeseBig0 <luca.ostuni.lima@gmail.com>
2025-08-29 22:18:49 -0400

    Update Challenge.java

--- Challenge.java
+++ Challenge.java
@@ -1,20 +1,3 @@
-public String ceaserCypher(String encoded, int distance)
-    {
-        private String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        encoded.toUpperCase();
-
-        String decodedString = "";
-
-        for (int i = 0; i < alphabet.length; i++)
-        {
-            if ((i + distance) < 26)
-            {
-                decodedString += alphabet[(i + distance)];
-            } else
-            {
-                decodedString += alphabet[(i + distance) - 26];
-            }
-        }
-        
-        return decodedString;
-    }
+public static void main(String[] args) {
+    System.out.println("HELLO")
+}
